e05dcdd48cd2df2a1745ebc654af1704cef8767c
Anreas Maerki <andy1111@me.com>
2013-06-30 20:47:20 +0200

    Initial Commit

---
+++ NetworkDiscoverer/Interface1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SnmpSharpNet;
+
+
+
+namespace NetworkDiscoverer
+{
+    interface IController
+    {
+        protected String startIPAddress;
+        protected String endIPAddress;
+        protected String passwd;
+        protected String community;
+        protected int netmask;
+
+        public String EndIPAddress
+        {
+            get { return endIPAddress; }
+            //set { endIPAddress = value; }
+        }
+
+        public int Netmask
+        {
+            get { return netmask; }
+            //set { netmask = value; }
+        }
+
+        public String Community
+        {
+            get { return community; }
+            //set { community = value; }
+        }
+
+        public String Passwd
+        {
+            get { return passwd; }
+            //set { passwd = value; }
+        }
+
+
+        public String StartIPAddress
+        {
+            get { return startIPAddress; }
+            //set { startIPAddress = value; }
+        }
+
+    }
+}
